6acb735991c75b020ceb3b636f6ca7734e74b9e1
Srabani Saha <79473283+yesthedaydreamer@users.noreply.github.com>
2022-05-06 19:53:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        appendDots: '.slider-dots',
+        dotsClass: 'dots'
+
+    });
+
+
+
+
+
+});
